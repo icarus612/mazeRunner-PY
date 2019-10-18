e784d054b12c5fa7a81044bc22a865534097235c
icarus <ellishogan95@gmail.com>
2019-10-17 22:42:43 -0400

    first commit

---
+++ maze.py
@@ -0,0 +1,25 @@
+
+
+class Maze:
+	def __init__(self, maze):
+		self.length = len(maze[0])
+		self.height = len(maze)
+		self.start
+		self.end
+		self.current_path
+		self.visited
+		self.wall = []
+		self.open = []
+	for x in range(maze):
+		for y in range(maze[x]):
+			p = maze[x][y]
+			if p == " ":
+				self.open = (x, y)
+			elif p == "#":
+				self.wall = (x, y)
+			elif p == "s"
+				self.start = (x, y)
+			elif p == "e":
+				self.end = (x, y)
+
+
