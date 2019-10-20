fc8c90282345cb87ed6c53877c146148f74c9216
icarus <ellishogan95@gmail.com>
2019-10-19 20:27:34 -0400

    update

--- maze.py
+++ maze.py
@@ -1,11 +1,12 @@
 import random
+
 class Maze:
-	def __init__ (self, maze=[]):
-		self.width = None
-		self.height = None
+	def __init__ (self, maze=[[]]):
+		self.width = len(maze[0])
+		self.height = len(maze)
+		self.maze = maze
 		self.start = None
 		self.end = None
-		self.maze = maze
 		
 	def make_maze(self, height, width, maze_type = "h"):
 		self.height = height
@@ -20,39 +21,47 @@ class Maze:
 				else:
 					open_points.append((x,y))
 		if maze_type == "h":
-			self.start = (1, random.choice(range(1, width-1)))
-			self.end = (height-2, random.choice(range(1, width-1)))
-			del open_points[open_points.index(self.start)]
-			del open_points[open_points.index(self.end)]
+			s= (1, random.choice(range(1, width-1)))
+			e = (height-2, random.choice(range(1, width-1)))
+			del open_points[open_points.index(s)]
+			del open_points[open_points.index(e)]
 		elif maze_type == "v":
-			self.start = (random.choice(range(1, height-1)), 1)
-			self.end = (random.choice(range(1, height-1)), width-2)
-			del open_points[open_points.index(self.start)]
-			del open_points[open_points.index(self.end)]
-		elif maze_type == "fse":
-			self.start = random.choice(open_points)
-			del open_points[open_points.index(self.start)]
-			self.end = random.choice(open_points)
-			del open_points[open_points.index(self.end)]
+			s = (random.choice(range(1, height-1)), 1)
+			e = (random.choice(range(1, height-1)), width-2)
+			del open_points[open_points.index(s)]
+			del open_points[open_points.index(e)]
+		elif maze_type == "r":
+			s = random.choice(open_points)
+			del open_points[open_points.index(s)]
+			e = random.choice(open_points)
+			del open_points[open_points.index(e)]
 		else:
-			raise ValueError("Incorrect Maze type. (try h, v, or fse)")
-		self.maze[self.start[0]][self.start[1]] = "s"
-		self.maze[self.end[0]][self.end[1]] = "e"
+			raise ValueError("Incorrect Maze type. (try h, v, or r)")
+		self.maze[s[0]][s[1]] = "s"
+		self.maze[e[0]][e[1]] = "e"
 		for i in open_points:
 			self.maze[i[0]][i[1]] = random.choice([" ", " ", "#"])
-					
-	def find_end_points(self, maze):
-		for x in range(len(maze)):
-			for y in range(len(maze[x])):
-				p = maze[x][y]
+
+	def see_maze(self):
+		for i in self.maze:
+			print(i)
+	
+	def find_end_points(self):
+		for x in range(len(self.maze)):
+			for y in range(len(self.maze[x])):
+				p = self.maze[x][y]
 				if p == "s" or p == "S":
 					self.start = (x, y)
 				elif p == "e" or p == "E":
 					self.end = (x, y)
 
-	def see_maze(self):
-		for i in self.maze:
-			print(i)
+	def runner(self):
+		self.find_end_points()
+		current_route = []
+		checked_paths = []
+		possible_routes = []
+		print(self.maze[self.start[0]][self.start[1]])
 
 	def maze_stats(self):
-		print(f"	start: {self.start} \n	end: {self.end}\n	size: {self.height} x {self.length}")
+		self.find_end_points()
+		print(f"	start: {self.start} \n	end: {self.end}\n	size: {self.height} x {self.width}")
