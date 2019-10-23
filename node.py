ba33a4348bba878bc11bb9cbc829f61277fcec16
icarus <ellishogan95@gmail.com>
2019-10-22 23:10:38 -0400

    getting there

--- node.py
+++ node.py
@@ -7,11 +7,4 @@ class Node:
 		self.children.add(child_node) 
 		
 	def remove_child(self, child_node):
-		self.children = [child for child in self.children if child is not child_node]
-
-	def traverse(self):
-		nodes_to_visit = [self]
-		while len(nodes_to_visit) > 0:
-			current_node = nodes_to_visit.pop()
-			print(current_node.value)
-			nodes_to_visit += current_node.children
+		self.children.discard(child_node)
--- runner.py
+++ runner.py
@@ -1,18 +1,20 @@
 from node import Node
 class Runner:
 	def __init__(self, maze):
-		self.open_nodes = []
+		self.open_nodes = set()
 		self.node_paths = []
 		self.start = None
 		self.end = None
 		self.maze = maze
+		self.find_end_points()
+		self.get_open_nodes()
 
 	def get_open_nodes(self):
 		p = self.maze.layout
 		for x in range(len(p)):
 			for y in range(len(p[x])):
 				if p[x][y] == self.maze.open_char:
-					self.open_nodes.append(Node((x, y)))
+					self.open_nodes.add(Node((x, y)))
 
 	def find_end_points(self):
 		for x in range(len(self.maze.layout)):
@@ -33,11 +35,9 @@ class Runner:
 				node.add_child(i)
 			elif i.value[1]+1 == node.value[1] and i.value[0] == node.value[0]:
 				node.add_child(i)
-		for i in node.children:
-			print(i.value)
-
-	def make_node_paths(self):
-		self.find_end_points()
-		self.get_open_nodes()
-		print(self.start.value)
-		self.look_around(self.start)
+		
+	def make_node_paths(self, point):
+		self.look_around(point)
+		for i in point.children:
+			self.make_node_paths(i)
+			
