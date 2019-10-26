ee000b32ff05084659b34bd5c944fa3b87fba370
icarus <ellishogan95@gmail.com>
2019-10-26 19:14:44 -0400

    stack overflow branch

--- node.py
+++ node.py
@@ -1,14 +1,22 @@
+import copy
+
 class Node:
 	def __init__(self, value):
 		self.value = value 
-		self.children = set()
-		self.path = set()
+		self.children = []
+		self.visited = []
 
+	def add_visited(self, node):
+		self.visited.append(copy.deepcopy(node))	
+	
 	def add_child(self, child_node):
-		self.children.add(child_node) 
+		self.children.append(copy.deepcopy(child_node))
 		
 	def add_path(self, node_path):
-		self.path = node_path
+		if not self.path:
+			self.path = node_path
+		else:
+			self.path = self.path if len(self.path) < len(node_path) else node_path
 
 	def remove_child(self, child_node):
 		self.children.discard(child_node)
