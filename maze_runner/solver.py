8931c990259f05ddbc2b316fdd2715f0b547c7a5
icarus64 <ellishogan95@gmail.com>
2024-02-25 15:40:00 -0500

    update

--- maze_runner/solver.py
+++ maze_runner/solver.py
@@ -1,5 +1,7 @@
-from maze_runner import Maze, Runner
+import argparse
 import re
+
+from . import Maze, Runner
 
 def open_and_build(file):
 	with open(file) as m:
@@ -21,14 +23,6 @@ def open_and_build(file):
 		end = flat[1]
 		return Maze(m1, start, end, wall, space)
 
-def find_type():
-	maze_type = input("Would you like to upload a maze or build a new one? \n  1) Build New \n  2) Upload \n").strip()
-	try:
-		return int(maze_type)
-	except:
-		input("Type must be a number. (press enter to try again ctrl + c to end)")
-		return find_type()
-
 def upload():
 	upload_file = input("Choose a file to upload: ").strip()
 	try:
@@ -37,13 +31,25 @@ def upload():
 		input("File Not found. (press enter to try again ctrl + c to end)")
 		return upload()
 
-def build_new():
-	maze_info = input("Enter maze height and width: ").strip()
-	try:
-		m = [int(i) for i in maze_info.split(" ")]
-	except:
-		input("Try again (Both hieght and with must be numbers: height width) (press enter to try again ctrl + c to end)")
+def find_type():
+	while True:
+		maze_type = input("Would you like to upload a maze or build a new one? \n  1) Build New \n  2) Upload \n").strip()
+		try:
+			m_type = int(maze_type)
+			if m_type not in [1, 2]:
+				input("Choose 1 or 2. (press enter to try again ctrl + c to end)")
+				continue
+		except:
+			input("Type must be a number. (press enter to try again ctrl + c to end)")
+			continue
+		break
+	
+	if m_type == 1:
 		return build_new()
+	else:
+		return upload()
+		
+def place_points(m):
 	while True:
 		point_placement = input("Where would you like your start and end points to be placed in the maze? \n  1) Top and bottom \n  2) Left and Right \n  3) Random Placement \n").strip()
 		try:
@@ -63,12 +69,20 @@ def build_new():
 	maze = Maze()
 	maze.build_new(m[0], m[1], maze_type)
 	return maze
-	
 
-def save_file(file_name):
+def build_new():
+	maze_info = input("Enter maze height and width: ").strip()
+	try:
+		m = [int(i) for i in maze_info.split(" ")]
+	except:
+		input("Try again (Both hieght and with must be numbers: height width) (press enter to try again ctrl + c to end)")
+		return build_new()
+	return place_points(m)
+
+def save_file(file_name, runner):
 	with open(file_name, "w") as file:
 		file.write("Origional maze: \n")
-		for i in maze.layout:
+		for i in runner.maze.layout:
 			for j in i:
 				file.write(j)
 			file.write("\n")
@@ -79,30 +93,36 @@ def save_file(file_name):
 			file.write("\n")
 		file.write("\n")
 
-m_type = find_type()
-while True:
-	if (m_type == 1):
-		maze = build_new()
-		break
-	elif (m_type == 2):
-		maze = upload()
-		break
-	else:
-		input("Choose 1 or 2. (press enter to try again ctrl + c to end)")
-		m_type = find_type()
+def build_and_save(runner, file_name):
+	runner.make_node_paths()
+	complete = "Yes" if runner.completed else "No"
+	print(f"Is maze possible? {complete}")
+	if runner.completed:	
+		runner.build_path()
+		runner.view_completed()
+		save_file(file_name, runner)
+	
+ 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-of", "--openfile", help="Open File Name", dest='openfile', type=str)
+	parser.add_argument("-sf", "--savefile", help="Save File Name", dest='savefile', type=str, default='completed.txt')
+	parser.add_argument("-d", "--dimensions", help="Maze dimentions", dest='dimensions', type=int, nargs=2)
+	parser.add_argument("-t", "--type", help="Maze Type", dest='type', type=str, default='h')
+	args = parser.parse_known_args()
 
-maze.view_layout()
-runner = Runner(maze)
-runner.make_node_paths()
-complete = "Yes" if runner.completed else "No"
-print(f"Is maze possible? {complete}")
+	m_type = args.type
+	if m_type not in ['h', 'v', 'r']:
+		m_type = place_points()
 
-if runner.completed:	
-	runner.build_path()
-	runner.view_completed()
-	file = input("What would you like to name this '.txt' file? \nFor default (completed.txt) press enter. ").strip()
-	if re.match(r"^[\w\-. ]+$", file):
-		save_file(f"{file}.txt")
+	if args.openfile:
+		maze = open_and_build(args.openfile, m_type)
+	elif args.dimensions:
+		maze = Maze(build=(int(args.dimensions[0]), int(args.dimensions[1])), build_type=m_type)	
 	else:
-		save_file("completed.txt")
+		maze = find_type()
+
+	maze.view_layout()
+	runner = Runner(maze)
+	build_and_save(runner, "completed.txt")
 	
